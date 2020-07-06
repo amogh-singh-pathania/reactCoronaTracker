20c49dd53b72c0744a4048be093338c5599ac0ec
a1bhi2 <agaguptaji@gmail.com>
2020-07-07 00:24:55 +0530

    Cards added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
+import { fetchData } from './api';
+import { Cards , Chart , CountryPicker } from './components';
+
+class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+
+    this.setState({ data:fetchedData });
+    
+  }
+  render(){
+    const { data } = this.state;
+    return (
+    <div className={styles.container} >
+      < Cards data={data} />
+      < CountryPicker />
+      < Chart />
     </div>
   );
+}
 }
 
 export default App;
