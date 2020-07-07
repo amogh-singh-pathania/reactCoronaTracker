a84f4ca1341dd682e2c3553badec0cf9640dbf5b
a1bhi2 <agaguptaji@gmail.com>
2020-07-08 03:02:04 +0530

    Updates

---
+++ precache-manifest.267da6225c2269048f492e963c41892c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe9b4f4112ff582b2562f69a2a04208",
+    "url": "/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "f4128832070594d7560f",
+    "url": "/react-corona-tracker/static/css/main.b136fb04.chunk.css"
+  },
+  {
+    "revision": "76c1a21ad3dfc4027d0f",
+    "url": "/react-corona-tracker/static/js/2.7a9754c7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona-tracker/static/js/2.7a9754c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4128832070594d7560f",
+    "url": "/react-corona-tracker/static/js/main.cbe5af1a.chunk.js"
+  },
+  {
+    "revision": "cd06ae12231c84d4f78f",
+    "url": "/react-corona-tracker/static/js/runtime-main.6115be80.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona-tracker/static/media/image.d7265326.png"
+  }
+]);
